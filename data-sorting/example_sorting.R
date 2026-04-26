98304c7553deaf6e8a7566f44101b585e38a39d9
JoelHellmanCode <joel1.hellman@gmail.com>
2026-04-26 22:41:40 +0200

    Delete data-sorting directory

--- data-sorting/example_sorting.R
+++
@@ -1,7 +0,0 @@
-# Data Sorting Scripts
-# Add your R data sorting/processing scripts here
-
-# Example: Sort and clean data
-# data <- read.csv("../data/raw_data.csv")
-# sorted_data <- data[order(data$column_name), ]
-# write.csv(sorted_data, "../data/sorted_data.csv", row.names = FALSE)
