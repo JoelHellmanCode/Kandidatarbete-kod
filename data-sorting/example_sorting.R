df91898df3483185baddca5ffc08bf766040e847
JoelHellmanCode <joel1.hellman@gmail.com>
2026-04-26 22:32:55 +0200

    Create example_sorting.R with data sorting template

---
+++ data-sorting/example_sorting.R
@@ -0,0 +1,7 @@
+# Data Sorting Scripts
+# Add your R data sorting/processing scripts here
+
+# Example: Sort and clean data
+# data <- read.csv("../data/raw_data.csv")
+# sorted_data <- data[order(data$column_name), ]
+# write.csv(sorted_data, "../data/sorted_data.csv", row.names = FALSE)
